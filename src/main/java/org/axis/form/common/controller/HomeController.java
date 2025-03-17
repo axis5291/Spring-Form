525952e19314796061ab254bf39b75e1c40a5bf6
Axis <erlia@naver.com>
2025-03-18 02:38:52 +0900

    3

--- src/main/java/org/axis/form/common/controller/HomeController.java
+++ src/main/java/org/axis/form/common/controller/HomeController.java
@@ -1,2 +1,14 @@
-package org.axis.form.common.controller;public class HomeController {
+package org.axis.form.common.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "Hello Form Controller";
+    }
+
 }
